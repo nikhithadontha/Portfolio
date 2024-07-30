8aa337dc820c0e4350a3a2921d27d75d3419c431
Nikhitha Dontha <nikhithadontha@gmail.com>
2024-07-30 23:36:22 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,122 @@
+const logotext = "Nikhitha Dontha";
+const meta = {
+    title: "Nikhitha Dontha",
+    description: "I’m Nikhitha Dontha _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Nikhitha Dontha",
+    animated: {
+        first: "Mastery of both front-end and back-end technologies",
+        second: "Transforming Ideas into Reality with Code",
+        third: "Dynamic Developer, Creative Problem Solver",
+    },
+    description: "As a passionate Full Stack Developer, I specialize in creating robust and scalable web applications. With expertise in front-end and back-end technologies like JavaScript, React, Node.js, Express, MongoDB, and SQL, I deliver seamless user experiences and efficient solutions.",
+    your_img_url: "https://i.pinimg.com/564x/59/af/9c/59af9ce2696e848a807caf70e9514908.jpg",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "I excel in transforming complex problems into elegant, maintainable code. Whether it's single-page applications or full-stack projects, my work is high-quality, reliable, and user-friendly. I thrive in collaborative environments and am committed to continuous learning and innovation, ensuring every project exceeds expectations."
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "Ashritha",
+        date: "2024",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Suresh",
+        date: "2024",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Supriya",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Spring Boot",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Mongo DB",
+        value: 85,
+    },
+    {
+        name: "Node Js",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.pinimg.com/236x/29/d1/8f/29d18f401c26da49d14b33da7a37de27.jpg",
+        description: " Created a responsive music player with interactive controls and dynamic visuals, showcasing advanced front-end development skills and Web Audio API integration.",
+        link: "#",
+    },
+    {
+        img: "https://i.pinimg.com/564x/a5/97/68/a59768c0d56dd015525f062e0df78db5.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.pinimg.com/236x/3d/10/92/3d10925d2b290304216427974b30f365.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+  
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nikhithadontha@gmail.com",
+    YOUR_FONE: "9553539598",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
